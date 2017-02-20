3967b98226cfaf7508ad2fbeddabd3580478b092
unknown <laigc@u-e-b.com>
2017-02-20 18:24:40 +0800

    update

---
+++ src/Veda/Amazon/Config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laigc
+ * Date: 2017/2/20
+ * Time: 18:23
+ */
+
+namespace Veda\Amazon;
+
+
+class Config
+{
+
+}
---
+++ src/Veda/Amazon/RequestAbstract.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laigc
+ * Date: 2017/2/20
+ * Time: 18:24
+ */
+
+namespace Veda\Amazon;
+
+
+abstract class RequestAbstract
+{
+
+}
